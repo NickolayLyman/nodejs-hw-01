0dbae20090c766f73501131bb47581d08041caa3
Nickolay Lyman <66874848+NickolayLyman@users.noreply.github.com>
2021-04-29 21:03:24 +0300

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,24 +1,31 @@
-const fs = require('fs');
-const path = require('path');
-const contactsPath = path.join(__dirname, './db/contacts.json');
+import fs from "fs";
+import path from "path";
+
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const contactsPath = path.join(resolve(__dirname, "./db/contacts.json"));
 
 function listContacts() {
   fs.readFile(contactsPath, (err, data) => {
     if (err) {
-      console.error(err);
+      console.log(err);
     }
     const rawData = data.toString();
+
     if (!rawData) {
       process.exit(1);
     }
 
-    const contactsList = JSON.parse(rawData);
+    const contactList = JSON.parse(rawData);
 
-    if (contactsList.length === 0) {
-      console.log('Contacts list is empty');
+    if (contactList.length === 0) {
+      console.log("Contact list is empty!");
       return;
     }
-    console.table(contactsList);
+    console.table(contactList);
   });
 }
 
@@ -27,14 +34,16 @@ function getContactById(contactId) {
     if (err) {
       console.error(err);
     }
+
     const rawData = data.toString();
+
     if (!rawData) {
       process.exit(1);
     }
 
-    const contactsList = JSON.parse(rawData);
+    const contactList = JSON.parse(rawData);
 
-    const foundContact = contactsList.find(({ id }) => id === contactId);
+    const foundContact = contactList.find(({ id }) => id === contactId);
 
     if (foundContact) {
       console.table([foundContact]);
@@ -49,25 +58,26 @@ function removeContact(contactId) {
     if (err) {
       console.error(err);
     }
+
     const rawData = data.toString();
+
     if (!rawData) {
       process.exit(1);
     }
 
-    const contactsList = JSON.parse(rawData);
+    const contactList = JSON.parse(rawData);
 
-    const filteredContacts = contactsList.filter(({ id }) => id !== contactId);
+    const filteredContacts = contactList.filter(({ id }) => id !== contactId);
 
-    if (contactsList.length !== filteredContacts.length) {
-      fs.writeFile(contactsPath, JSON.stringify(filteredContacts), err => {
+    if (contactList.length !== filteredContacts.length) {
+      fs.writeFile(contactsPath, JSON.stringify(filteredContacts), (err) => {
         if (err) {
           console.error(err.message);
           process.exit(1);
         }
       });
     }
-
-    console.log('Contact was deleted successfully');
+    console.log("Contact was deleted successfully!");
     listContacts();
   });
 }
@@ -79,36 +89,39 @@ function addContact(name, email, phone) {
     }
 
     const rawData = data.toString();
-    let contactsList;
+
+    let contactslist;
     let id;
 
     if (!rawData) {
-      contactsList = [];
+      contactslist = [];
       id = 1;
     } else {
-      contactsList = JSON.parse(rawData);
+      contactslist = JSON.parse(rawData);
       id =
-        contactsList.length === 0
+        contactslist.length === 0
           ? 1
-          : contactsList[contactsList.length - 1].id + 1;
+          : contactslist[contactslist.length - 1].id + 1;
     }
 
     if (name && email && phone) {
-      contactsList.push({ id, name, email, phone });
-      fs.writeFile(contactsPath, JSON.stringify(contactsList), err => {
+      contactslist.push({ id, name, email, phone });
+      fs.writeFile(contactsPath, JSON.stringify(contactslist), (err) => {
         if (err) {
           console.error(err);
         }
-        console.log('Contact was added and saved successfully');
-        console.table(contactsList);
+        console.log("Contacts was added and saved successfuly");
+        console.table(contactslist);
       });
     }
   });
 }
 
-module.exports = {
+const contactsAction = {
   listContacts,
   removeContact,
   addContact,
   getContactById,
 };
+
+export default contactsAction;
